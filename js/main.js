379dfc34ba97bb11cb5be01d4df8e79b04e295c2
Brian <brianparkerin@gmail.com>
2020-06-17 05:55:39 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+  });
+  
+  ScrollReveal().reveal('.home-cards');
+  ScrollReveal().reveal('.xbox', { delay: 500 });
+  ScrollReveal().reveal('.home-cards', { delay: 500 });
+  ScrollReveal().reveal('.carbon', { delay: 500 });
+  ScrollReveal().reveal('.follow', { delay: 500 });
+  //ScrollReveal().reveal('.links', { delay: 500 });
+  ScrollReveal().reveal('.footer', { delay: 500 });
